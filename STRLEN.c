24a4ea4b3a8247d006830e3b7e28e107b5ab7a5c
RNVKB256 <99805705+RNVKB256@users.noreply.github.com>
2022-06-05 11:50:55 +0530

    Update STRLEN.c

--- STRLEN.c
+++ STRLEN.c
@@ -1,5 +1,5 @@
 // string length without using library function and w/o spacing
-/*#include<stdio.h>
+#include<stdio.h>
 int main()
 {
 	char string[20],length,i=0;
@@ -12,7 +12,7 @@ int main()
 	printf("length of the string is =%d",i);
 	return 0;
 }
-*/
+
 // when there is space in the word how to find the length
 #include<stdio.h>
 int main ()
